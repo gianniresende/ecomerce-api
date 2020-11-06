c45294f5ca8fa03746fe9247a6aa8fb5d7964125
gianniresende <gianniresende@gmail.com>
2020-11-06 17:06:33 -0300

    feat: create models category and system_requiriment, create first tests

---
+++ app/models/system_requiriment.rb
@@ -0,0 +1,10 @@
+class SystemRequiriment < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates :operational_system, presence: true
+  validates :storage, presence: true
+  validates :processor, presence: true
+  validates :memory, presence: true
+  validates :video_board, presence: true  
+  
+end

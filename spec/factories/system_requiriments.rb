19105c71fb6e97514c872147790239d028df2dec
gianniresende <gianniresende@gmail.com>
2020-11-06 18:57:01 -0300

    feat: add model product and first rspec tests

--- spec/factories/system_requiriments.rb
+++ spec/factories/system_requiriments.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :system_requiriment do
     sequence(:name) { |n| "Basic #{n}" }
-    operational_system { "MyString" }
-    storage { "MyString" }
-    processor { "MyString" }
-    memory { "MyString" }
-    video_board { "MyString" }
+    operational_system { Faker::Computer.os }
+    storage { "500 GB" }
+    processor { "AMD Ryzen 7" }
+    memory { "8 GB" }
+    video_board { "GeForce MX 9600" }
   end
 end

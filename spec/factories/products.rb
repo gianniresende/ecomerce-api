e31e9570679bcd517761a3f81d9507e831432bb9
gianniresende <gianniresende@gmail.com>
2020-11-10 20:49:58 -0300

    feat: add tests of requests

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,12 @@ FactoryBot.define do
     sequence(:name) { |n| "MyString #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
+    image { 
+            Rack::Test::UploadedFile.new(
+              Rails.root.join("spec/support/images/product_image.png")
+            ) 
+          }
+
     after :build do |product|
       product.productable = create(:game)
     end

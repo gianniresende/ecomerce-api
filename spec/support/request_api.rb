8abb99df075cb00021d767e1e1024da2c5a99108
gianniresende <gianniresende@gmail.com>
2020-11-20 16:21:10 -0300

    feat: add crud and tests coupon

--- spec/support/request_api.rb
+++ spec/support/request_api.rb
@@ -1,5 +1,4 @@
 module RequestAPI
-
   def body_json(symbolize_keys: false)
     json = JSON.parse(response.body)
     symbolize_keys ? json.deep_symbolize_keys : json
@@ -13,7 +12,6 @@ module RequestAPI
     header = auth.merge({ 'Content-Type' => 'application/json', 'Accept' => 'application/json' })
     header.merge merge_with
   end
-
 end
 
 RSpec.configure do |config|

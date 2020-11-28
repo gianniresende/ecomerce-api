ba476dc7245019c1976c6d0c7288e41e8f2f522c
gianniresende <gianniresende@gmail.com>
2020-11-28 15:02:46 -0300

    feat: add column status to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,13 @@ class Product < ApplicationRecord
   include NameSearchable
   include Paginatable
   
+  enum status: { available: 1, unavailable: 2 }
+
   validates :name, presence: true, uniqueness: { case_sensitive: false } 
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0}
   validates :image, presence: true
+  validates :status, presence: true
   
   belongs_to :productable, polymorphic: true
   has_many :product_categories, dependent: :destroy
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "MyString #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
+    status { :available }
     image { 
             Rack::Test::UploadedFile.new(
               Rails.root.join("spec/support/images/product_image.png")
